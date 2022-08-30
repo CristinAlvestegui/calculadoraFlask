376624296c5e8bb225c416ee2de9f4cd47718fe1
CRISTINA ALVESTEGUI SUBIETA <cristina.asubieta@senacsp.edu.br>
2022-08-29 22:10:53 -0300

    first commit

--- main.py
+++ main.py
@@ -1,18 +1,74 @@
-from flask import Flask, render_template
+from crypt import methods
+import re
+from unittest import result
+from flask import Flask, render_template #Importando bibliotecas Flask
 from flask import request, redirect
+from operacoes import operacoes #importanto nossa classe operações
+import this
+
 
 app = Flask(__name__)#Referência ao objeto Flask, crinado uma variavel APP que guarda os elementos do Flask
-@app.route("/")#Pagina index - priemira página de qualquer site
+ope = operacoes()#nossa chave para abrir o classe operacoes
 
+this.resultadoFinal = ""
+
+@app.route("/")#Pagina index - priemira página de qualquer site, tipo uma rota para uma pagina
 def homepage():
     return render_template("index.html")
 
-@app.route("/soma", methods=['POST','GET'])
+@app.route("/soma", methods=['POST','GET']) #aqui estamos fazendo um rota para a página /soma, com os métodos GET ou POST
 def soma():
     if request.method == 'POST':
-        num1 = request.form['num1']#dentro do [] vai o nome da variavel, name no HTML
-        num2 = request.form['num2']
+        numero1 = request.form['num1']#dentro do [] vai o nome da variavel, name no HTML
+        numero2 = request.form['num2']
+        this.resultadoFinal = ope.somar(numero1,numero2)
+    return render_template("soma.html", titulo="Soma", resultado=this.resultadoFinal)
 
+@app.route("/subtrair", methods=['POST', 'GET'])
+def subt():
+    if request.method == 'POST':
+        numero1 = request.form['num1']
+        numero2 = request.form['num2']
+        this.resultadoFinal = ope.subtrair(numero1, numero2)
+    return render_template("subtrair.html", titulo="Subtração", resultado=this.resultadoFinal)
+
+@app.route("/multi", methods=['POST', 'GET'])
+def mult():
+    if request.method == 'POST':
+        numero1 = request.form['num1']
+        numero2 = request.form['num2']
+        this.resultadoFinal = ope.multiplicar(numero1, numero2)
+    return render_template("multi.html", titulo="multiplicar", resultado=this.resultadoFinal)
+
+@app.route("/dividir", methods=['POST', 'GET'])
+def divir():
+    if request.method == 'POST':
+        numero1 = request.form['num1']
+        numero2 = request.form['num2']
+        this.resultadoFinal = ope.dividir(numero1, numero2)
+    return render_template("dividir.html", titulo="dividir", resultado=this.resultadoFinal)
+
+@app.route("/potencia", methods=['POST', 'GET'])
+def poten():
+    if request.method == 'POST':
+        numero1 = request.form['num1']
+        numero2 = request.form['num2']
+        this.resultadoFinal = ope.potencia(numero1, numero2)
+    return render_template("potencia.html", titulo="Potência", resultado=this.resultadoFinal)
+
+@app.route("/raiz", methods=['POST', 'GET'])
+def raizes():
+    if request.method == 'POST':
+        numero1 = request.form['num1']
+        this.resultadoFinal = ope.rais(numero1)
+    return render_template("raiz.html", titulo="Raiz", resultado=this.resultadoFinal)
+
+@app.route("/tabuada", methods=['POST', 'GET'])
+def tabu():
+    if request.method == 'POST':
+        numero1 = request.form['num1']
+        this.resultadoFinal = ope.tabuada(numero1)
+    return render_template("tabuada.html", titulo="Tabuada", resultado=this.resultadoFinal)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
--- operacoes.py
+++ operacoes.py
@@ -6,33 +6,33 @@ class operacoes:
         pass
 
     def somar(self, num1, num2):
-        return num1 + num2
+        return float(num1) + float(num2)
 
     def subtrair(self, num1, num2):
-        return num1 - num2
+        return float(num1) - float(num2)
 
     def multiplicar(self, num1, num2):
-        return num1 * num2
+        return float(num1) * float(num2)
 
     def dividir(self, num1, num2):
         if num2 == 0:
             return  "Impossível dividir por zero!"
         else:
-            return num1 / num2
+            return float(num1) / float(num2)
 
     def potencia(self, base, expoente):
-        if expoente == 0:
+        if float(expoente) == 0:
             return 1
-        elif expoente == 1:
-            return base
+        elif float(expoente) == 1:
+            return float(base)
         else:
             return math.pow(base, expoente)
 
     def rais(self, num):
-        if num < 0:
+        if float(num) < 0:
             return "Impossível calcular"
         else:
-            return math.sqrt(num)
+            return math.sqrt(float(num))
 
     def tabuada(self, num):
         for i in range(11):
